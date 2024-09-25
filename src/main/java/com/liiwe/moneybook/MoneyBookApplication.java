79d2f6c1f7af7cb434ed755467aacc090169496f
heart14 <lwf14@qq.com>
2024-09-25 18:31:28 +0800

    feat: init project

---
+++ src/main/java/com/liiwe/moneybook/MoneyBookApplication.java
@@ -0,0 +1,15 @@
+package com.liiwe.moneybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wfli
+ * @since 2024/9/25 18:26
+ */
+@SpringBootApplication
+public class MoneyBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyBookApplication.class, args);
+    }
+}
