6a9c802cea8f1e938ad422c2ca25dd10714dfb7f
heart14 <lwf14@qq.com>
2024-10-18 16:35:12 +0800

    feat: 记账接口

--- src/main/java/com/liiwe/moneybook/controller/DailyRecordController.java
+++ src/main/java/com/liiwe/moneybook/controller/DailyRecordController.java
@@ -9,10 +9,7 @@ import com.liiwe.moneybook.base.listener.RecordExcelListener;
 import com.liiwe.moneybook.service.MoneyBookService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -39,6 +36,16 @@ public class DailyRecordController {
      */
     @PostMapping("/save")
     public MoneyBookRecord save(@Valid @RequestBody SaveRecordRequest request) {
+        return moneyBookService.save(request);
+    }
+
+    /**
+     * 保存记账记录
+     * @param request
+     * @return
+     */
+    @GetMapping("/record")
+    public MoneyBookRecord record(@Valid SaveRecordRequest request) {
         return moneyBookService.save(request);
     }
 
