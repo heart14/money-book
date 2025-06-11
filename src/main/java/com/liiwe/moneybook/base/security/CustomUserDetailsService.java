67e556766c229233f730c64e7a36ed41c22cc26e
heart14 <lwf14@qq.com>
2025-06-11 15:34:19 +0800

    fix: spring security对请求进行认证时进行处理角色信息

--- src/main/java/com/liiwe/moneybook/base/security/CustomUserDetailsService.java
+++ src/main/java/com/liiwe/moneybook/base/security/CustomUserDetailsService.java
@@ -1,15 +1,21 @@
 package com.liiwe.moneybook.base.security;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.liiwe.moneybook.base.bean.entity.SysRole;
 import com.liiwe.moneybook.base.bean.entity.SysUser;
+import com.liiwe.moneybook.mapper.SysRoleMapper;
 import com.liiwe.moneybook.mapper.SysUserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 自定义用户详情服务类
@@ -18,14 +24,18 @@ import java.util.Collections;
  * @since 2025/6/10 15:28
  */
 @Service
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 
     // 注入用户映射器
     private final SysUserMapper userMapper;
 
+    private final SysRoleMapper roleMapper;
+
     // 构造函数，用于依赖注入
-    public CustomUserDetailsService(SysUserMapper userMapper) {
+    public CustomUserDetailsService(SysUserMapper userMapper, SysRoleMapper roleMapper) {
         this.userMapper = userMapper;
+        this.roleMapper = roleMapper;
     }
 
     // 重写 UserDetailsService 接口的 loadUserByUsername 方法
@@ -43,7 +53,18 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("用户名或密码错误");
         }
 
-        // 返回一个 Spring Security 的 User 对象，包含用户名、密码和权限列表（这里权限列表为空）
-        return new User(sysUser.getUsername(), sysUser.getPassword(), Collections.emptyList());
+        // 处理用户角色信息
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        List<SysRole> sysRoles = roleMapper.selectUserRoleByUid(sysUser.getUid());
+        if (sysRoles != null && !sysRoles.isEmpty()) {
+            for (SysRole sysRole : sysRoles) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + sysRole.getRole().toUpperCase()));
+            }
+        }
+
+        log.info("user role info: {}", authorities);
+        // 返回一个 Spring Security 的 User 对象，包含用户名、密码和角色列表
+        return new User(sysUser.getUsername(), sysUser.getPassword(), authorities);
     }
 }
