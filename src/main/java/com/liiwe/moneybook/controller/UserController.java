f79ba63ae0855f17212d9f55d54947f92b319e15
heart14 <lwf14@qq.com>
2025-06-11 15:34:37 +0800

    fix: 修改冗余import

--- src/main/java/com/liiwe/moneybook/controller/UserController.java
+++ src/main/java/com/liiwe/moneybook/controller/UserController.java
@@ -8,7 +8,6 @@ import com.liiwe.moneybook.service.SysUserService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
