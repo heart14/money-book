5a1f98f2c2ce877983232292aa37f904e42c2f3c
heart14 <lwf14@qq.com>
2025-06-11 16:12:26 +0800

    feat: 新增查询用户信息接口响应类

---
+++ src/main/java/com/liiwe/moneybook/base/bean/domain/user/UserInfo.java
@@ -0,0 +1,35 @@
+package com.liiwe.moneybook.base.bean.domain.user;
+
+import com.liiwe.moneybook.base.bean.entity.SysRole;
+import com.liiwe.moneybook.base.bean.entity.SysUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author wfli
+ * @since 2025/6/11 15:46
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    private Long uid;
+
+    private String username;
+
+    private String nickname;
+
+    private List<String> roles;
+
+    public UserInfo(SysUser sysUser, List<String> roles) {
+        this.uid = sysUser.getUid();
+        this.username = sysUser.getUsername();
+        this.nickname = sysUser.getNickname();
+        this.roles = roles;
+    }
+
+}
