5e49dcf2358d0b2ed201a13c478136d538564910
heart14 <lwf14@qq.com>
2025-06-20 16:30:38 +0800

    feat: 新增分页查询收支列表请求入参类

---
+++ src/main/java/com/liiwe/moneybook/base/bean/domain/mb/PageListReq.java
@@ -0,0 +1,33 @@
+package com.liiwe.moneybook.base.bean.domain.mb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author wfli
+ * @since 2025/6/19 14:59
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageListReq {
+
+    private int current;
+
+    private int size;
+
+    private String date;
+
+    private String dateRangeStart;
+
+    private String dateRangeEnd;
+
+    private String title;
+
+    private String type;
+
+    private Integer categoryId;
+
+    private String path;
+}
