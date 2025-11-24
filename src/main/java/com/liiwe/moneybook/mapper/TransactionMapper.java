d7b01524cdac722f77c9f85b1444a296ac83ad38
heart14 <lwf14@qq.com>
2025-11-24 15:04:25 +0800

    feat: 新增批量导入trans数据库接口

--- src/main/java/com/liiwe/moneybook/mapper/TransactionMapper.java
+++ src/main/java/com/liiwe/moneybook/mapper/TransactionMapper.java
@@ -3,6 +3,9 @@ package com.liiwe.moneybook.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.liiwe.moneybook.base.bean.entity.Transaction;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author lwf14
@@ -10,4 +13,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface TransactionMapper extends BaseMapper<Transaction> {
+
+    void batchInsertTransaction(@Param("list") List<Transaction> transList);
 }
