131a4c59643bf318691e07c3a4530c3854c9bdf5
heart14 <lwf14@qq.com>
2025-11-24 15:03:01 +0800

    fix: 优化数据导入接口

--- src/main/java/com/liiwe/moneybook/controller/SystemController.java
+++ src/main/java/com/liiwe/moneybook/controller/SystemController.java
@@ -4,17 +4,11 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 import com.alibaba.excel.EasyExcel;
 import com.liiwe.moneybook.base.bean.domain.mb.MoneyBookReq;
-import com.liiwe.moneybook.base.bean.entity.DiaryBook;
-import com.liiwe.moneybook.base.bean.entity.MoneyBook;
-import com.liiwe.moneybook.base.bean.model.MoneyBookImportTemplate;
+import com.liiwe.moneybook.base.bean.model.MoneyBookTemplate;
 import com.liiwe.moneybook.base.bean.model.SysResponse;
 import com.liiwe.moneybook.base.listener.MoneyBookImportListener;
-import com.liiwe.moneybook.base.utils.CategoryMappingUtil;
-import com.liiwe.moneybook.service.DiaryBookService;
-import com.liiwe.moneybook.service.MoneyBookService;
-import com.liiwe.moneybook.service.SysCategoryService;
+import com.liiwe.moneybook.service.biz.DataImportService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * @author wfli
@@ -33,36 +26,23 @@ import java.util.List;
 @Slf4j
 public class SystemController {
 
-    private final MoneyBookService moneyBookService;
-    private final DiaryBookService diaryBookService;
-    private final CategoryMappingUtil categoryMappingUtil;
+    private final DataImportService dataImportService;
 
-    public SystemController(MoneyBookService moneyBookService, DiaryBookService diaryBookService,  CategoryMappingUtil categoryMappingUtil) {
-        this.moneyBookService = moneyBookService;
-        this.diaryBookService = diaryBookService;
-        this.categoryMappingUtil = categoryMappingUtil;
+    public SystemController(DataImportService dataImportService) {
+        this.dataImportService = dataImportService;
     }
 
     @PostMapping("/import")
     public SysResponse importFromExcel(@RequestBody MultipartFile file, String sheet) {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
-
         log.info("test import, {}", file.getOriginalFilename());
-
-        MoneyBookImportListener listener = new MoneyBookImportListener(categoryMappingUtil);
+        MoneyBookImportListener listener = new MoneyBookImportListener();
         try {
-            EasyExcel.read(file.getInputStream(), MoneyBookImportTemplate.class, listener).sheet(sheet).doRead();
+            EasyExcel.read(file.getInputStream(), MoneyBookTemplate.class, listener).sheet(sheet).doRead();
         } catch (IOException e) {
             throw new RuntimeException("模板导入失败");
         }
-
-        List<MoneyBook> moneyBookList = listener.getMoneyBookList(name);
-        List<DiaryBook> diaryBookList = listener.getDiaryBookList(name);
-
-        moneyBookService.importMoneyBook(moneyBookList);
-        diaryBookService.importDiaryBook(diaryBookList);
-
-        return SysResponse.success(moneyBookList);
+        dataImportService.excel(listener.getRecords());
+        return SysResponse.success();
     }
 
     @PostMapping("/screen")
@@ -76,22 +56,17 @@ public class SystemController {
         }
         log.info("time:{}", parse);
 
-        int categoryId = categoryMappingUtil.getCategoryId(category);
-
 
         MoneyBookReq moneyBookReq = new MoneyBookReq();
         moneyBookReq.setTitle(title);
         moneyBookReq.setAmount(amount);
         moneyBookReq.setDatetime(DateUtil.format(parse, "yyyy-MM-dd hh:mm:ss"));
         moneyBookReq.setType(type);
-        moneyBookReq.setCategoryId(categoryId);
+//        moneyBookReq.setCategoryId(categoryId);
         moneyBookReq.setRemark(remark);
         moneyBookReq.setUsername(username);
 
         log.info("save from mobile:{}", moneyBookReq);
-
-        moneyBookService.saveMoneyBook(moneyBookReq, true);
-
         return SysResponse.success();
     }
 }
