7679831a5cb285d8a7062aac457e0a47aacf016f
heart14 <lwf14@qq.com>
2025-12-10 18:21:37 +0800

    fix: 添加注释

--- src/main/java/com/liiwe/moneybook/controller/UserController.java
+++ src/main/java/com/liiwe/moneybook/controller/UserController.java
@@ -1,9 +1,8 @@
 package com.liiwe.moneybook.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.liiwe.moneybook.base.bean.domain.manage.RoleInfo;
-import com.liiwe.moneybook.base.bean.domain.mb.PageUserReq;
 import com.liiwe.moneybook.base.bean.domain.manage.UserInfo;
+import com.liiwe.moneybook.base.bean.domain.mb.PageUserReq;
 import com.liiwe.moneybook.base.bean.model.SysResponse;
 import com.liiwe.moneybook.service.base.SysUserService;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +26,11 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 获取当前登录用户信息
+     *
+     * @return SysResponse
+     */
     @GetMapping("/info")
     public SysResponse getUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -36,8 +40,14 @@ public class UserController {
         return SysResponse.success(userInfo);
     }
 
+    /**
+     * 分页查询所有用户列表
+     *
+     * @param req 分页查询参数
+     * @return SysResponse
+     */
     @GetMapping("/list")
-    public SysResponse getUserList(PageUserReq req){
+    public SysResponse getUserList(PageUserReq req) {
         Page<UserInfo> list = userService.getUserList(req);
         return SysResponse.success(list);
     }
